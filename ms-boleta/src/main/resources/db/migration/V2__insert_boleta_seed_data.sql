46b3f5b579d306ba27a5d3b07c4a8e3e8f40137d
Kenneth-Araya <karayaavalos@gmail.com>
2026-05-16 01:19:39 -0400

    Se modifican directorios para mayor orden y mejora visual para el github

---
+++ ms-boleta/src/main/resources/db/migration/V2__insert_boleta_seed_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO bd_boletas.boleta
+(folio_boleta, glosa_boleta, tipo_boleta, fecha_emision_boleta, monto_bruto_boleta, monto_neto_boleta, estado_boleta)
+VALUES
+('BLT-1001','Compra de insumos de oficina','AFECTA','2026-03-01',119000,100000,'EMITIDA'),
+('BLT-1002','Servicio de mantención de equipos','AFECTA','2026-03-03',238000,200000,'PAGADA'),
+('BLT-1003','Adquisición de materiales eléctricos','AFECTA','2026-03-05',59500,50000,'PENDIENTE'),
+('BLT-1010','Compra de útiles escolares','EXENTA','2026-03-18',38000,38000,'PENDIENTE');
+
