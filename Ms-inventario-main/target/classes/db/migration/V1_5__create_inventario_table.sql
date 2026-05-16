c7f46574abd16249a59c77539fd2fe989aebce44
Kenneth-Araya <karayaavalos@gmail.com>
2026-05-16 00:57:00 -0400

    Se elimina carpeta sistema market ya que los ms estan independientes

---
+++ Ms-inventario-main/target/classes/db/migration/V1_5__create_inventario_table.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS bd_inventario;
+
+CREATE TABLE IF NOT EXISTS bd_inventario.inventario (
+    id_inventario BIGSERIAL PRIMARY KEY,    
+    id_producto BIGINT NOT NULL,         
+    stock_actual INT NOT NULL DEFAULT 0, 
+    stock_minimo INT NOT NULL DEFAULT 0,
+    fecha_vencimiento DATE              
+);
+
