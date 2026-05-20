3c6dab20533a07ea0349a1f632fc283f38dce9bd
Kenneth-Araya <karayaavalos@gmail.com>
2026-05-20 04:41:34 -0400

    Se arreglan problemas con el pom y se cambia el valor del atributo int ya que hacia problema con el postgtres al iniciar la bd

--- ms-usuario/src/main/java/cl/duoc/sistemaMarket/model/Usuario.java
+++ ms-usuario/src/main/java/cl/duoc/sistemaMarket/model/Usuario.java
@@ -20,7 +20,7 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false, length = 100)
     private String nombreUsuario;
